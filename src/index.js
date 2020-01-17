d18fe22e82f171a08958215589134923310635e5
tundeiness <tundeiness@yahoo.co.uk>
2020-01-17 15:56:52 +0100

    remove unwanted codes

--- src/index.js
+++ src/index.js
@@ -60,15 +60,6 @@ class Board extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if(winner){
-      status = `Winner ${ winner }`
-    }else{
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-    // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
     return (
       <div>
         <div className="status">{status}</div>
