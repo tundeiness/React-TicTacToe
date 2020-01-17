36dbdd49615a18ef93a067dc3d157b2fa600930a
tundeiness <tundeiness@yahoo.co.uk>
2020-01-17 14:05:13 +0100

    changed status display

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
+    squares[i] = this.state.xIsNext ?  'X' : 'O';
     this.setState({
       squares:squares,
       xIsNext: !this.state.xIsNext,
@@ -65,7 +65,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
